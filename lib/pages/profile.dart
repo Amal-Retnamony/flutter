82de96b3e905c30a82d69030c955ce693b305b28
amalr <amalr@qburst.com>
2021-05-01 19:06:57 +0530

    Added changes

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -10,8 +10,44 @@ class Profile extends StatelessWidget {
         backgroundColor: Colors.blue,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Icon(Icons.person),
+          Icon(Icons.person, size: 200, color: Colors.grey,),
+          SizedBox(height: 10),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(8),
+                  child: Text('Name', style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  )),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(32,8, 16, 8),
+                  child: Text('User', style: TextStyle(
+                    fontSize: 16,
+                  )),
+                ),
+                Container(
+                  margin: EdgeInsets.all(8),
+                  child: Text('Email', style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  )),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(32,8, 16, 8),
+                  child: Text('user@gmail.com', style: TextStyle(
+                    fontSize: 16,
+                  )),
+                ),
+              ],
+            )
+          )
         ],
       )
     );
